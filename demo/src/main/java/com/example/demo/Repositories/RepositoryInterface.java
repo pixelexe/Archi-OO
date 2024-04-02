5c55eaa1e09fbb1912ea5b497b28a708bb82fc79
pixelexe <rerezheng@gmail.com>
2024-04-02 19:45:56 +0200

    patch le bug que mon camarade a crée

--- demo/src/main/java/com/example/demo/Repositories/RepositoryInterface.java
+++ demo/src/main/java/com/example/demo/Repositories/RepositoryInterface.java
@@ -3,5 +3,5 @@ package com.example.demo.Repositories;
 public interface RepositoryInterface {
     String DB_URL = "jdbc:mysql://localhost:3306/aoo";
     String DB_USERNAME = "root";
-    String DB_PASSWORD = "root";
+    String DB_PASSWORD = "";
 }
